86c83173780cf258fc145e5469676d8f52032836
Adam Hicks <ahicksaccess@gmail.com>
2026-09-19 19:42:01 -0400

    Add NixOS flake configuration

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Adam's NixOS system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }@inputs: {
+    nixosConfigurations = {
+      # Replace 'hostname' with your actual hostname
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.adam = import ./home.nix;
+          }
+          hyprland.nixosModules.default
+          { programs.hyprland.enable = true; }
+        ];
+      };
+    };
+  };
+}
